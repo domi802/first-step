09c3363bb44730f89d6f064506024e95c4a1dc51
domi802 <64065747+domi802@users.noreply.github.com>
2022-01-06 00:23:10 +0100

    Initial Commit

---
+++ udemy/udemyApp.swift
@@ -0,0 +1,17 @@
+//
+//  udemyApp.swift
+//  udemy
+//
+//  Created by Dominik Woźniak on 06/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct udemyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
